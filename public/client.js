fb53abf51eb8b7f8c49d594bc7d45092de35808b
Herschel TUIL <herschel.tuil@gmail.com>
2022-10-11 21:19:15 +0200

    First commit

--- public/client.js
+++ public/client.js
@@ -3,24 +3,52 @@ import { OrbitControls } from './jsm/controls/OrbitControls.js'
 import Stats from './jsm/libs/stats.module.js'
 import { GUI } from './jsm/libs/lil-gui.module.min.js'
 
-const scene = new THREE.Scene()
+//scène
+const scene = new THREE.Scene();
+
+scene.background = new THREE.CubeTextureLoader()
+    .setPath('/assets/')
+    .load([
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg'
+    ]);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.z = 2
+camera.position.z = 5
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+// Terre
+const earthGeometry = new THREE.SphereGeometry(1);
+const load = new THREE.TextureLoader().load("/assets/earthmap1k.jpg");
+const bumpEarth = new THREE.TextureLoader().load("/assets/earthbump1k.jpg");
+const earthMaterial = new THREE.MeshLambertMaterial({
+    map: load,
+    bump: bumpEarth,
+    transparent: false
+});
+const earth = new THREE.Mesh(earthGeometry, earthMaterial)
+scene.add(earth);
+
+//Sphère Céleste
+const geometry = new THREE.SphereGeometry(1.6, 18, 18);
+const material = new THREE.MeshBasicMaterial({ wireframe: true }, { wireframeLineWidth: 0.01 });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+//Lumière
+const light = new THREE.AmbientLight(0x404040, 2.5); // soft white light
+scene.add(light);
+
+
 
 window.addEventListener(
     'resize',
@@ -33,23 +61,27 @@ window.addEventListener(
     false
 )
 
+
+
+
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
 const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.scale, 'x', -5, 5)
-cubeFolder.add(cube.scale, 'y', -5, 5)
-cubeFolder.add(cube.scale, 'z', -5, 5)
-cubeFolder.open()
+const sphereFolder = gui.addFolder('sphere')
+sphereFolder.add(sphere.scale, 'x', -5, 5)
+sphereFolder.add(sphere.scale, 'y', -5, 5)
+sphereFolder.add(sphere.scale, 'z', -5, 5)
+sphereFolder.open()
 const cameraFolder = gui.addFolder('Camera')
 cameraFolder.add(camera.position, 'z', 0, 10)
 cameraFolder.open()
 
 function animate() {
     requestAnimationFrame(animate)
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    sphere.rotation.y += 0.001
+    earth.rotation.y += 0.001
+    // il va falloir faire tourner le point pour l'afficher comme il faut
     controls.update()
     render()
     stats.update()
